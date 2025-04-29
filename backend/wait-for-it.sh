8fb74d88fd656ddc807532cdcc98c0cf03bd18b1
Victor <victor.oberto@gmail.com>
2025-04-29 10:58:45 -0300

    Readme and db management updated.

--- backend/wait-for-it.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Check if the database has started
-# We could also use ${POSTGRES_PORT:-5432} to provide a default value
-if [ -n "$POSTGRES_HOST" ] && [ -n "$POSTGRES_PORT" ]
-then
-    /usr/local/bin/wait-for-it "$POSTGRES_HOST:$POSTGRES_PORT" --strict --timeout=10
-fi
